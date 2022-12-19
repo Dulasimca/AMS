e7c258f83460401cfb4c1a22f56f1ed716c54036
ananth61 <ananth.murugan16@gmail.com>
2022-12-19 11:06:32 +0530

    brand master edit update

--- src/app/CommonModules/PathConstants.ts
+++ src/app/CommonModules/PathConstants.ts
@@ -1,8 +1,9 @@
 export class PathConstants {
     public static readonly itregister_Post = 'itregister/Post';
     public static readonly ItRegisterMaster_Get='ItRegisterMaster/GetItRegister';
-    public static readonly productmaster_Post = 'productmaster/Post';
-    public static readonly brandmaster_Post = 'brandmaster/Post';
+    public static readonly productmaster_Post ='productmaster/Post';
+    public static readonly brandmaster_Post ='brandmaster/Post';
     public static readonly brandmaster_Get='brandmaster';
+    public static readonly updatebrandmaster_Post='updatebrandmaster/Post';
 
 }
--- src/app/master/brand-master/brand-master.component.ts
+++ src/app/master/brand-master/brand-master.component.ts
@@ -7,54 +7,56 @@ import { RestAPIService } from 'src/app/restapi.service';
   templateUrl: './brand-master.component.html',
   styleUrls: ['./brand-master.component.css']
 })
+
 export class BrandMasterComponent implements OnInit {
   brandname: any;
   selectedType: any;
   data: any[] = [];
   cols: any;
   branddata: any[] = [];
-  brandOptions:any;
-  brandid: any;
+  brandOptions: any;
+  brandid:any;
 
   constructor(private restApiService: RestAPIService) { }
 
   ngOnInit(): void {
     this.onView();
-    this.onSave();
+    this.brandid=0;
+    
 
     this.cols = [
-      { field: 'brandname', header: 'brandname', align: 'left !important' },
-      { field: 'createdate', header: 'createdate', align: 'left !important' },
-      { field: 'flag', header: 'flag', align: 'right !important' },
+      { field: 'brandname', header: 'Brandname', align: 'left !important' },
+      { field: 'createdate', header: 'Createddate', align: 'left !important' },
+      { field: 'flag', header: 'Flag', align: 'right !important' },
     ]
 
   }
-  onSignIn() {
 
+  onSignIn() {
+    if(this.brandid==0){
     const params = {
-      'brandid': 0,
+      'brandid': this.brandid,
       'brandname': this.brandname,
       'flag': (this.selectedType == 1) ? true : false
     }
-    this.restApiService.post(PathConstants.brandmaster_Post, params).subscribe(res => {
-     
-
-    })
+    this.restApiService.post(PathConstants.brandmaster_Post, params).subscribe(res => { })
+    this.onView();
+    
+  }
+   else{
+    const params = {
+      'brandid': this.brandid,
+      'brandname': this.brandname,
+      'flag': (this.selectedType === '1') ? true : false,
+    }
+    this.restApiService.post(PathConstants.updatebrandmaster_Post, params).subscribe(res => { })
 
   }
   
-
-  onSave(){
+}
+  onView() {
     this.restApiService.get(PathConstants.brandmaster_Get).subscribe(res => {
-      if(res){
-      this.branddata = res.Table;
-  
-      }
-    }) 
-  }
-
-  onView(){
-    this.restApiService.get(PathConstants.brandmaster_Get).subscribe(res => { this.data = res.Table;
+      this.data = res.Table;
     })
 
   }
@@ -63,14 +65,13 @@ export class BrandMasterComponent implements OnInit {
     this.brandname = null;
     this.selectedType = null;
     this.brandid = 0;
-
   }
-onEdit(rowData:any){
-  this.brandid = rowData.brandid;
-  this.brandname = rowData.brandname;
-  this.selectedType = (rowData.flag === 'Active') ? 1 : 0;
+
+  onEdit(rowData: any) {
+    this.brandid = rowData.brandid;
+    this.brandname = rowData.brandname;
+    this.selectedType = (rowData.flag === 'true') ? 1 : 0;
+  }
 
 }
-
-  }
 
